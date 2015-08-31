f43c49b0ec23bc86322dd0604fcde85fff372979
Marcos Thomaz <eu@marcosthomaz.com>
2015-08-31 15:32:39 -0300

    Alteracao no frame

--- GameTree/src/gametree/GameTree.java
+++ GameTree/src/gametree/GameTree.java
@@ -1,32 +1,53 @@
 package gametree;
 
-import java.awt.FlowLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 /**
  *
  * @author thomaz
  */
-public class GameTree {
+public class GameTree extends JFrame {
+
+    public GameTree() {
+        initComponents();
+        super.setSize(700, 400);
+        super.setTitle("GameTree - COMP2014");
+        super.setResizable(false);
+        super.setLocationRelativeTo(null);
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
-        JFrame frame = new JFrame("GameTree - COMP");
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-
-        frame.add(panel);
-        frame.setSize(700, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setResizable(false);
-
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GameTree().setVisible(true);
+            }
+        });
     }
 
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
 }
